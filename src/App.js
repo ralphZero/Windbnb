5f881ed030a47b5f93ef063e4c20e7cc4bbfc352
ralphZero <placideralph@gmail.com>
2020-12-08 17:20:17 -0500

    data fetched

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import Grid from './components/Grid';
 import Navbar from './components/Navbar';
 import StaysBar from './components/StaysBar';
+import json_data from './assets/stays.json';
 
 class App extends Component {
+
+  state = {
+    data : null
+  }
+
+  componentDidMount(){
+    let json = JSON.stringify(json_data)
+    let array = JSON.parse(json)
+    this.setState({
+      data : array
+    })
+  }
 
   render() {
     return (
       <div>
         <Navbar />
         <StaysBar />
-        <Grid />
+        <Grid data={this.state.data}/>
       </div>
     );
   }
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,52 +1,33 @@
 import sheet from './Grid.module.css'
 
-const Grid = (props) => {
+const Grid = ({data}) => {
+    
+    const dataList = data ? (
+        data.map(stay => {
+            const superHost = stay.superHost ? (<span className={sheet.stay_info_superhost}>{'super host'.toUpperCase()}</span>) : null
+            
+            return (
+                <div key={Math.random()} className={sheet.stay_item}>
+                    <div className={sheet.stay_img_wrapper}>
+                        <img className={sheet.stay_img} src={stay.photo} alt="stay"/>
+                    </div>
+                    <div className={sheet.stay_info}>
+                        {superHost}
+                        <span className={sheet.stay_info_type}>{stay.type}</span>
+                        <span className={sheet.stay_info_rating}>
+                            <span className={'material-icons '+sheet.stay_info_rating_star}>star</span>
+                            <span>{stay.rating}</span>
+                        </span>
+                    </div>
+                    <div className={sheet.stay_desc}>{stay.title}</div>
+                </div>
+            )
+        })
+    ) : null
+
     return (
         <div className={sheet.container}>
-            <div className={sheet.stay_item}>
-                <div className={sheet.stay_img_wrapper}>
-                    <img className={sheet.stay_img} src="https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" alt="stay"/>
-                </div>
-                <div className={sheet.stay_info}>
-                    <span className={sheet.stay_info_superhost}>{'super host'.toUpperCase()}</span>
-                    <span className={sheet.stay_info_type}>Entire apartment . 2 beds</span>
-                    <span className={sheet.stay_info_rating}>
-                        <span className={'material-icons '+sheet.stay_info_rating_star}>star</span>
-                        <span>4.40</span>
-                    </span>
-                </div>
-                <div className={sheet.stay_desc}>Stylist apartment in center of the city</div>
-            </div>
-
-            <div className={sheet.stay_item}>
-                <div className={sheet.stay_img_wrapper}>
-                    <img className={sheet.stay_img} src="https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" alt="stay"/>
-                </div>
-                <div className={sheet.stay_info}>
-                    <span className={sheet.stay_info_superhost}>{'super host'.toUpperCase()}</span>
-                    <span className={sheet.stay_info_type}>Entire apartment . 2 beds</span>
-                    <span className={sheet.stay_info_rating}>
-                        <span className={'material-icons '+sheet.stay_info_rating_star}>star</span>
-                        <span>4.40</span>
-                    </span>
-                </div>
-                <div className={sheet.stay_desc}>Stylist apartment in center of the city</div>
-            </div>
-
-            <div className={sheet.stay_item}>
-                <div className={sheet.stay_img_wrapper}>
-                    <img className={sheet.stay_img} src="https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" alt="stay"/>
-                </div>
-                <div className={sheet.stay_info}>
-                    <span className={sheet.stay_info_superhost}>{'super host'.toUpperCase()}</span>
-                    <span className={sheet.stay_info_type}>Entire apartment . 2 beds</span>
-                    <span className={sheet.stay_info_rating}>
-                        <span className={'material-icons '+sheet.stay_info_rating_star}>star</span>
-                        <span>4.40</span>
-                    </span>
-                </div>
-                <div className={sheet.stay_desc}>Stylist apartment in center of the city</div>
-            </div>
+            {dataList}
         </div>
     )
 }
